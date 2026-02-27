056e06fd1c636501c38d3e04c3097ea0af571fee
dawn545 <2091109554@qq.com>
2026-02-27 16:38:59 +0800

    Add orientation field control methods to Kobayashi3D

--- Kobayashi3D.h
+++ Kobayashi3D.h
@@ -6,6 +6,7 @@
 
 const float PI_F = 3.14159265358979f;
 
+
 class Kobayashi3D
 {
 public:
@@ -24,12 +25,17 @@ public:
     void togglePause() { _updateFlag = !_updateFlag; }
     bool isPaused() const { return !_updateFlag; }
 
+    // 取向场控制接口
+    void resetOrientationField() { _resetOrientationField(); }
+    void applyVortexField() { _applyVortexField(); }
+    void applyVortexField(bool clockwise) { _applyVortexField(clockwise); }
+    void paintOrientation(int screenX, int screenY, float angle, float radius = 20.0f, float blendFactor = 0.6f);
+
     // 相机控制
     void rotateCamera(float deltaX, float deltaY);
     void zoomCamera(float delta);
 
 private:
-    // 模拟参数
     struct int2 { int x; int y; };
     int2 _objectCount = { 0, 0 };
     inline int _INDEX(int i, int j) { return (i + _objectCount.x * j); };
@@ -37,20 +43,21 @@ private:
     float _dx, _dy, _dt;
     float _tau, _epsilonBar, _mu, _K, _delta, _anisotropy, _alpha, _gamma, _tEq;
 
-    // 相场数据
     std::vector<float> _phi, _t, _epsilon, _epsilonDeriv, _gradPhiX, _gradPhiY, _lapPhi, _lapT, _angl;
+    std::vector<double> _orientationField;  // Ω(x,y) - 局部优先生长方向 [0, 2π]
 
-    // OpenGL 纹理和渲染
+
+    // OpenGL 纹理和渲染（用于2D切片可视化）
     std::vector<unsigned char> _pixelBuffer;
     GLuint _textureID = 0;
     bool _updateFlag = true;
+
 
     // 3D 相机参数
     float _cameraRotX = 30.0f;
     float _cameraRotY = 45.0f;
     float _cameraDistance = 3.0f;
-
-    // 厚度参数
+    
     float _crystalThickness = 0.03f;  // 冰晶最大厚度
 
     // 内部方法
@@ -61,4 +68,8 @@ private:
     void _evolution();
     void _updateTexture();
     void _render3DCrystal();
+
+    // 取向场管理 (Ren et al. 2018)
+    void _resetOrientationField();      // 重置为全局统一方向
+    void _applyVortexField(bool clockwise = false);  // 应用涡旋场（2D兼容）
 };
